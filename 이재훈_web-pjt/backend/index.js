ec5a357183cd301a1618d81f9ac409329a810b55
garoserokk <95258090+garoserokk@users.noreply.github.com>
2022-09-16 17:46:49 +0900

    Add files via upload

---
+++ 이재훈_web-pjt/backend/index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+
+const app = express();
+const PORT = 8081;
+
+const cors = require("cors");
+app.use(cors());
+
+const morgan = require("morgan");
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+const axios = require("axios");
+const dotenv = require("dotenv");
+const fs = require("fs");
+dotenv.config();
+
+app.get("/data", (req, res) => {
+  try {
+    res.set("Content-Type", "application/json; charset=utf-8");
+    const tempFile = fs.createReadStream("uploads/chart.json");
+    return tempFile.pipe(res);
+  } catch (error) {
+    return res.json(error);
+  }
+});
+
+app.post("/data", async (req, res) => {
+  const { startDate, endDate, timeUnit, device, gender, keywordGroups } =
+    req.body;
+  try {
+    const request_body = {
+      startDate: startDate,
+      endDate: endDate,
+      timeUnit: timeUnit,
+      device: device === "all" ? "" : device,
+      gender: gender === "all" ? "" : gender,
+      keywordGroups: keywordGroups,
+    };
+
+    const url = "https://openapi.naver.com/v1/datalab/search";
+    const headers = {
+      "Content-Type": "application/json",
+      "X-Naver-Client-Id": process.env.CLIENT_ID,
+      "X-Naver-Client-Secret": process.env.CLIENT_SECRET,
+    };
+    const response = await axios.post(url, request_body, {
+      headers: headers,
+    });
+    fs.writeFile(
+      `./uploads/chart.json`,
+      JSON.stringify(response.data.results),
+      (error) => {
+        if (error) {
+          throw error;
+        }
+      }
+    );
+    return res.json(response.data.results);
+  } catch (error) {
+    return res.json(error);
+  }
+});
+
+app.listen(PORT, () => console.log(`this server listening on ${PORT}`));
---
+++ 이재훈_web-pjt/frontend/src/store/index.js
@@ -0,0 +1,13 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: { chartData: {} },
+  mutations: {
+    CHANGE_CHART_DATA(state, data) {
+      state.chartData = data;
+    },
+  },
+});
