8b604e9ebbaaa9cb9f5cc2f52223c02262f8bfd9
garoserokk <95258090+garoserokk@users.noreply.github.com>
2023-01-26 01:40:44 +0900

    Add files via upload

---
+++ project1/project/project/src/router/index.js
@@ -0,0 +1,11 @@
+import {createRouter, createWebHistory} from 'vue-router';
+
+
+const router = createRouter({
+    history :createWebHistory(),
+    routes:[]
+});
+
+// 1/home  2/todos  3/ todos/create 4/todos/:id
+
+export default router;
